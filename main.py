252999139ad5511cff0555ddefc06cfd6848df71
mzeeshanaltaf <154883001+mzeeshanaltaf@users.noreply.github.com>
2024-05-28 18:31:08 +0500

    Added about information

--- main.py
+++ main.py
@@ -10,12 +10,17 @@ st.set_page_config(page_title=page_title, page_icon=page_icon, layout="centered"
 # Configure the API key
 st.sidebar.title("Config️uration Options")
 api_key, file_uploader = configure_apikey_sidebar()
+st.sidebar.divider()
+st.sidebar.subheader('About')
+st.sidebar.markdown(''' Any Queries: Contact [Zeeshan Altaf](zeeshan.altaf@gmail.com)''')
+st.sidebar.markdown(''' Source code: [GitHub](https://github.com/mzeeshanaltaf/csv-data-analysis)''')
 
 st.title("CSV Whisperer")
 st.write(":blue[***Unlock insights from your CSV data.***]")
 st.write("Transform the way you interact with your data. Simply upload your CSV file, "
          "and effortlessly process and explore its contents. You can ask any question "
          "related to your CSV data and receive instant, accurate answers")
+
 
 # Capture the CSV file
 st.subheader('Upload CSV File:')
